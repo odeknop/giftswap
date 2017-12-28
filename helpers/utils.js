1a814a6404b89a80ed52473f0e67c2a596772192
Olivier Deknop <odeknop@gmail.com>
2017-12-28 18:54:00 +0100

    add helpers

---
+++ helpers/utils.js
@@ -0,0 +1,10 @@
+module.exports = {
+	
+	encodeQueryData: function(data) {
+		let ret = []
+		for (let d in data)
+			ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]))
+		return ret.join('&')
+	},
+
+}
